8a125343fef0e43f674c86d6d904e2c977ad758c
Jared Hirsch <ohai@6a68.net>
2013-05-16 17:17:45 -0700

    yay more

--- cloudwatch2statsd.js
+++ cloudwatch2statsd.js
@@ -9,7 +9,7 @@ var cw = new CloudWatch({
     'region'          : amazon.US_WEST_2
 });
 
-function getHealthyHostCount(az, elb) {
+function getHealthyHostCount(cb, az, elb) {
   var startTime = new Date(new Date().getTime() - (24*1000*60*60));
   var endTime = new Date(new Date().getTime() - (1000*60));
   var az = az || 'us-west-2b';
@@ -30,13 +30,108 @@ function getHealthyHostCount(az, elb) {
   }, function(err, data) {
     // if (err) console.log(JSON.stringify(err, null, "  "));
     console.log(err, JSON.stringify(data, null, "  "));
+    cb(err, data);
   });
 }
 
-getHealthyHostCount('us-west-2a')
-getHealthyHostCount('us-west-2b')
-getHealthyHostCount('us-west-2c')
+function getHTTPCode_Backend_4XX(cb, az, elb) {
+  var startTime = new Date(new Date().getTime() - (24*1000*60*60));
+  var endTime = new Date(new Date().getTime() - (1000*60));
+  var az = az || 'us-west-2b';
+  var elb = elb || 'w-anosrep-org-0514';
+  
+  cw.GetMetricStatistics({
+    MetricName: 'HTTPCode_Backend_4XX',
+    Namespace: 'AWS/ELB',
+    Unit: ['Count'],
+    Period: 60,
+    Statistics: ['Sum'],
+    Dimensions: [
+      { Name: 'AvailabilityZone', Value: az },
+      { Name: 'LoadBalancerName', Value: elb }
+    ],
+    StartTime: startTime.toISOString('8601'),
+    EndTime: endTime.toISOString('8601')
+  }, function(err, data) {
+    // if (err) console.log(JSON.stringify(err, null, "  "));
+    console.log(err, JSON.stringify(data, null, "  "));
+    cb(err, data);
+  });
+}
 
+function getHTTPCode_Backend_5XX(cb, az, elb) {
+  var startTime = new Date(new Date().getTime() - (24*1000*60*60));
+  var endTime = new Date(new Date().getTime() - (1000*60));
+  var az = az || 'us-west-2b';
+  var elb = elb || 'w-anosrep-org-0514';
+  
+  cw.GetMetricStatistics({
+    MetricName: 'HTTPCode_Backend_5XX',
+    Namespace: 'AWS/ELB',
+    Unit: ['Count'],
+    Period: 60,
+    Statistics: ['Sum'],
+    Dimensions: [
+      { Name: 'AvailabilityZone', Value: az },
+      { Name: 'LoadBalancerName', Value: elb }
+    ],
+    StartTime: startTime.toISOString('8601'),
+    EndTime: endTime.toISOString('8601')
+  }, function(err, data) {
+    // if (err) console.log(JSON.stringify(err, null, "  "));
+    console.log(err, JSON.stringify(data, null, "  "));
+    cb(err, data);
+  });
+}
+
+function getHTTPCode_Backend_2XX(cb, az, elb) {
+  var startTime = new Date(new Date().getTime() - (24*1000*60*60));
+  var endTime = new Date(new Date().getTime() - (1000*60));
+  var az = az || 'us-west-2b';
+  var elb = elb || 'w-anosrep-org-0514';
+  
+  cw.GetMetricStatistics({
+    MetricName: 'HTTPCode_Backend_2XX',
+    Namespace: 'AWS/ELB',
+    Unit: ['Count'],
+    Period: 60,
+    Statistics: ['Sum'],
+    Dimensions: [
+      { Name: 'AvailabilityZone', Value: az },
+      { Name: 'LoadBalancerName', Value: elb }
+    ],
+    StartTime: startTime.toISOString('8601'),
+    EndTime: endTime.toISOString('8601')
+  }, function(err, data) {
+    // if (err) console.log(JSON.stringify(err, null, "  "));
+    console.log(err, JSON.stringify(data, null, "  "));
+    cb(err, data);
+  });
+}
+
+module.exports = {
+  getHealthyHostCount: getHealthyHostCount,
+  getHTTPCode_Backend_4XX: getHTTPCode_Backend_4XX,
+  getHTTPCode_Backend_5XX: getHTTPCode_Backend_5XX,
+  getHTTPCode_Backend_2XX: getHTTPCode_Backend_2XX
+};
+  
+
+// getHealthyHostCount('us-west-2a')
+//getHealthyHostCount('us-west-2b')
+//getHealthyHostCount('us-west-2c')
+
+//getHTTPCode_Backend_4XX('us-west-2a')
+//getHTTPCode_Backend_4XX('us-west-2b')
+//getHTTPCode_Backend_4XX('us-west-2c')
+
+//getHTTPCode_Backend_5XX('us-west-2a')
+//getHTTPCode_Backend_5XX('us-west-2b')
+//getHTTPCode_Backend_5XX('us-west-2c')
+
+getHTTPCode_Backend_2XX('us-west-2a')
+//getHTTPCode_Backend_2XX('us-west-2b')
+//getHTTPCode_Backend_2XX('us-west-2c')
 
 /*
 elb.find(cw, /^.*0502$/, function(err, elbs) {
